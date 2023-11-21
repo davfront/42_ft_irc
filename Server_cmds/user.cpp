ea608e9a8ab12b03a7e6d7526acff152c6897c87
davfront <contact@david-pereira.net>
2023-11-21 16:43:37 +0100

    remove useless todo

--- Server_cmds/user.cpp
+++ Server_cmds/user.cpp
@@ -6,7 +6,7 @@
 /*   By: dapereir <dapereir@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 10:54:00 by mmaxime-          #+#    #+#             */
-/*   Updated: 2023/11/07 13:54:59 by dapereir         ###   ########.fr       */
+/*   Updated: 2023/11/21 16:43:19 by dapereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@ void	Server::_user(Client & sender, std::vector<std::string> const & params)
 	if (params.size() < 4 || params[0].empty() || params[3].empty()) {
 		throw Server::ErrException(ERR_NEEDMOREPARAMS(sender.getNickname(), "USER"));
 	}
-
-	// todo: check params validity
-
+	
 	sender.setUsername("~" + params[0]);
 	sender.setRealname(params[3]);
 }
