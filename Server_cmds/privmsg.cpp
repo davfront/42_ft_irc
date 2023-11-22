edc10a52d657ccbf417ec67bd810007da6101cd7
davfront <contact@david-pereira.net>
2023-11-22 17:25:11 +0100

    [#76][feat] PRIVMSG to channel

--- Server_cmds/privmsg.cpp
+++ Server_cmds/privmsg.cpp
@@ -6,7 +6,7 @@
 /*   By: dapereir <dapereir@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 10:54:00 by mmaxime-          #+#    #+#             */
-/*   Updated: 2023/11/22 15:29:46 by dapereir         ###   ########.fr       */
+/*   Updated: 2023/11/22 17:00:02 by dapereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,28 @@ void	Server::_privmsg(Client & sender, std::vector<std::string> const & params)
 		throw Server::ErrException(ERR_NOTEXTTOSEND(sender.getNickname()));
 	}
 
-	Client* targetClient = this->_clients.get(params[0]);
-	if (!targetClient) {
-		throw Server::ErrException(ERR_NOSUCHNICK(sender.getNickname(), params[0]));
+	// parse target names
+	std::vector<std::string> targetNames = split(params[0], ",");
+	if (targetNames.empty()) {
+		throw Server::ErrException(ERR_NORECIPIENT(sender.getNickname(), "PRIVMSG"));
 	}
-	targetClient->reply(RPL_PRIVMSG(sender.getNickname(), targetClient->getNickname(), params[1]));
+
+	// for each target
+	for (size_t i = 0; i < targetNames.size(); i++) {
+		if (!targetNames[i].empty() && (targetNames[i][0] == '#' || targetNames[i][0] == '&')) {
+			// channel
+			Channel* channel = this->_channels.get(targetNames[i]);
+			if (!channel) {
+				throw Server::ErrException(ERR_NOSUCHNICK(sender.getNickname(), targetNames[i]));
+			}
+			channel->reply(RPL_PRIVMSG(sender.getNickname(), channel->getName(), params[1]));
+		} else {
+			// client
+			Client* client = this->_clients.get(targetNames[i]);
+			if (!client) {
+				throw Server::ErrException(ERR_NOSUCHNICK(sender.getNickname(), targetNames[i]));
+			}
+			client->reply(RPL_PRIVMSG(sender.getNickname(), client->getNickname(), params[1]));
+		}
+	}
 }
